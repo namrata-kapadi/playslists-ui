fb4eff8e6ad9c80742b3ece20de43579966a47ea
Namrata Kapadi <namratakapadi@gmail.com>
2020-10-28 15:06:06 +0000

    changes

--- playslists-ui/src/app/core/services/playslists.service.spec.ts
+++ playslists-ui/src/app/core/services/playslists.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { PlayslistsService } from './playslists.service';
 
 describe('PlayslistsService', () => {
   let service: PlayslistsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PlayslistsService]
+    });
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(PlayslistsService);
   });
 
--- playslists-ui/src/app/shared/components/playlist/playlist.component.spec.ts
+++ playslists-ui/src/app/shared/components/playlist/playlist.component.spec.ts
@@ -8,9 +8,9 @@ describe('PlaylistComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlaylistComponent ]
+      declarations: [PlaylistComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +19,4 @@ describe('PlaylistComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
